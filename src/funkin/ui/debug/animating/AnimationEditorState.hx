4ce1772ce45d18fbc0a047bd8a8c8aa8af0169c0
youbadao-sussy <youbadao@icloud.com>
2026-02-16 11:25:42 +0900

    a

--- src/funkin/ui/debug/animating/AnimationEditorState.hx
+++ src/funkin/ui/debug/animating/AnimationEditorState.hx
@@ -1,10 +1,11 @@
 package funkin.ui.debug.animating;
 
 import haxe.ui.backend.flixel.UIState;
+import haxe.ui.containers.VBox;
 import haxe.ui.events.MouseEvent;
 
 @:build(haxe.ui.ComponentBuilder.build("assets/exclude/data/ui/animation-editor/main-view.xml"))
-class AnimationEditorState extends UIState {
+class AnimationEditorState extends VBox {
     public function new() {
         super();
         button1.onClick = function(e) {
---
+++ src/funkin/ui/debug/animating/export/HorizonAtlasExport.hx
@@ -0,0 +1,9 @@
+package funkin.ui.debug.animating.export;
+
+class HorizonAtlasExport
+{
+    public function new()
+    {
+        
+    }
+}
