5c24cf31c798abdcf0a6f329d2bce9238773ecbf
youbadao-sussy <youbadao@icloud.com>
2026-02-18 14:34:50 +0900

    aaa

--- src/funkin/ui/debug/animating/AnimationEditorState.hx
+++ src/funkin/ui/debug/animating/AnimationEditorState.hx
@@ -1,8 +1,24 @@
 package funkin.ui.debug.animating;
 
+import haxe.ui.Toolkit;
 import haxe.ui.backend.flixel.UIState;
+import haxe.ui.components.Button;
+import haxe.ui.components.DropDown;
+import haxe.ui.components.Label;
+import haxe.ui.components.NumberStepper;
+import haxe.ui.components.Slider;
 import haxe.ui.containers.VBox;
+import haxe.ui.containers.dialogs.CollapsibleDialog;
+import haxe.ui.containers.menus.Menu;
+import haxe.ui.containers.menus.MenuBar;
+import haxe.ui.containers.menus.MenuCheckBox;
+import haxe.ui.containers.menus.MenuItem;
+import haxe.ui.core.Screen;
+import haxe.ui.events.DragEvent;
 import haxe.ui.events.MouseEvent;
+import haxe.ui.events.UIEvent;
+import haxe.ui.focus.FocusManager;
+import openfl.display.BitmapData;
 
 @:build(haxe.ui.ComponentBuilder.build("assets/exclude/data/ui/animation-editor/main-view.xml"))
 class AnimationEditorState extends VBox {
---
+++ src/funkin/ui/debug/animating/components/Menu.hx
@@ -0,0 +1,9 @@
+package funkin.ui.debug.animating.components;
+
+import haxe.ui.containers.menus.Menu;
+import haxe.ui.core.Screen;
+import haxe.ui.events.MouseEvent;
+
+class Menu extends View {
+    
+}
