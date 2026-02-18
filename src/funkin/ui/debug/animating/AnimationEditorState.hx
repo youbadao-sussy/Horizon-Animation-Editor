25bfba319b6d9d66c30645e6d6ce1b78a2af0e2f
youbadao-sussy <youbadao@icloud.com>
2026-02-18 19:31:35 +0900

    yea

--- src/funkin/ui/debug/animating/AnimationEditorState.hx
+++ src/funkin/ui/debug/animating/AnimationEditorState.hx
@@ -9,6 +9,7 @@ import haxe.ui.components.NumberStepper;
 import haxe.ui.components.Slider;
 import haxe.ui.containers.VBox;
 import haxe.ui.containers.dialogs.CollapsibleDialog;
+import haxe.ui.containers.dialogs.Dialogs;
 import haxe.ui.containers.menus.Menu;
 import haxe.ui.containers.menus.MenuBar;
 import haxe.ui.containers.menus.MenuCheckBox;
@@ -19,6 +20,10 @@ import haxe.ui.events.MouseEvent;
 import haxe.ui.events.UIEvent;
 import haxe.ui.focus.FocusManager;
 import openfl.display.BitmapData;
+import yaml.Parser;
+import yaml.Renderer;
+import yaml.Yaml;
+import yaml.util.ObjectMap;
 
 @:build(haxe.ui.ComponentBuilder.build("assets/exclude/data/ui/animation-editor/main-view.xml"))
 class AnimationEditorState extends VBox {
@@ -29,10 +34,22 @@ class AnimationEditorState extends VBox {
             button1.text = "Thanks!";
         }
     }
-    
-    @:bind(button2, MouseEvent.CLICK)
-    private function onMyButton(e:MouseEvent) {
-        button2.text = "Thanks!";
-    }
 	 */
+	public function new() {
+		super();
+	}
+
+	@:bind(help1, MouseEvent.CLICK)
+	private function onMyButton(e:MouseEvent) {
+		messageYee();
+	}
+	static function messageYee() {
+		#if sys
+		var data:AnyObjectMap = Yaml.read("assets/exclude/data/appData/main/about.yaml", Parser.options().useObjects());
+
+		Dialogs.messageBox(data.app + '\nVersion: ' + data.version + '\nPlatform: ' + data.platform + '\n' + '\nBuilt by: ' + data.created_by
+			+ '\nHaxeUI by: ' + data.haxeui + '\n\nnote' + data.description,
+			'About...', 'info');
+		#end
+	}
 }
