13e53bc58ab7dd379421e039687a2ca83a12d0cd
youbadao-sussy <youbadao@icloud.com>
2026-02-18 13:16:15 +0900

    a

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,6 @@
 package ;
 
+import funkin.ui.debug.animating.AnimationEditorState;
 import haxe.ui.HaxeUIApp;
 import haxe.ui.Toolkit;
 
@@ -7,7 +8,7 @@ class Main {
     public static function main() {
         var app = new HaxeUIApp();
         app.ready(function() {
-			app.addComponent(new MainView());
+			app.addComponent(new AnimationEditorState());
 
             app.start();
         });
